be2ea1db237266049914b1e000922e8faf552ffa
sydarn <sydarn@proton.me>
2025-01-14 19:54:18 +0100

    miyoo flip skeleton

---
+++ board/miyoo/flip_rk3566/flip-rk3566.c
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * Copyright (c) 2025 sydarn <sydarn@proton.me>
+ */
+
+void spl_board_init(void)
+{
+}
+
+int rk_board_late_init(void)
+{
+	return 0;
+}
---
+++ include/configs/miyoo-flip-rk3566.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef __MIYOO_FLIP_RK3566_H
+#define __MIYOO_FLIP_RK3566_H
+
+#include <configs/rk3568_common.h>
+
+#define ROCKCHIP_DEVICE_SETTINGS \
+			"stdout=serial,vidconsole\0" \
+			"stderr=serial,vidconsole\0"
+
+#endif
